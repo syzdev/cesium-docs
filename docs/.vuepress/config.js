00bbd2e6fff49db0b9f011292e2528cd00782eb8
syzdev <syzdev@gmail.com>
2021-08-30 11:40:30 +0800

    初始化首页

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '入门', link: '/guide/' },
+      { text: '关于', link: '/about/' },
+    ],
+    logo: '/assets/img/logo.png',
+  }
+}
